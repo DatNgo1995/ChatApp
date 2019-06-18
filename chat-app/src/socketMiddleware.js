8073950a237e24a961f420fc88f6bb29596166c0
datngo1995 <datngotat1995@gmail.com>
2019-06-19 02:33:54 +0300

    Create Web Socket middleware.

---
+++ chat-app/src/socketMiddleware.js
@@ -0,0 +1,11 @@
+export default function createSocketMiddleware(socket) {
+
+    return ({ dispatch, getState }) => (next) => (action) => {
+      if (typeof action === 'function') {
+        return action({ dispatch, socket, getState });
+      }
+  
+      return next(action);
+  
+    };
+  }

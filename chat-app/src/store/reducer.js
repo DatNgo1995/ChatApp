3ce5c89cc3bdf5b6b4eb7d3ef93b61b25470b596
datngo1995 <datngotat1995@gmail.com>
2019-06-19 02:34:48 +0300

    Implement Reducers.

---
+++ chat-app/src/store/reducer.js
@@ -0,0 +1,22 @@
+import C from '../constants';
+
+export const messages = (state=[],action) => {
+    switch (action.type) {
+        case C.POST_MESSAGE:
+            return [...state, action.payload]
+        case C.UPDATE_MESSAGE:
+            return state.map(message =>
+                message.id === action.payload.id
+                  ? { ...message, content: action.payload.content }
+                  : message
+              )
+        case C.DELETE_MESSAGE:
+            return state.filter( message => message.id != action.payload)
+        case C.GET_MESSAGE:
+                return action.payload
+    }
+}
+export const userList =  (state=[],action) => 
+    action.type === C. UPDATE_ONLINE_LIST ? action.payload : state
+
+export const currentPage = (state = [], action) => action.type = C.SET_PAGE ? action.payload : state

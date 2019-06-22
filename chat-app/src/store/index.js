9c0261e070e5ada0327ab105707a11ac0f2269ef
datngo1995 <datngotat1995@gmail.com>
2019-06-22 05:43:07 +0300

    Fix typos.

--- chat-app/src/store/index.js
+++ chat-app/src/store/index.js
@@ -1,9 +1,10 @@
 import C from '../constants'
-import appReducer from './reducers'
+import appReducer from './reducer'
+import socketIOClient from "socket.io-client";
 import thunk from 'redux-thunk'
-import createSocketMiddleware from 'socketMiddleware.js'
+import createSocketMiddleware from './socketMiddleware.js'
 import { createStore, applyMiddleware } from 'redux'
-const socketMiddleware = createSocketMiddleware(io('http://example.com/socket'));
+const socketMiddleware = createSocketMiddleware(socketIOClient.connect('/'));
 export default (initialState={}) => {
 	return applyMiddleware(thunk,socketMiddleware)(createStore)(appReducer, initialState)
 }

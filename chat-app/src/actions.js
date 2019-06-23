03c42bb92eff558889f8aceb04e85081097995fa
datngo1995 <datngotat1995@gmail.com>
2019-06-23 04:49:13 +0300

    Replace old functions with redux actions.

--- chat-app/src/actions.js
+++ chat-app/src/actions.js
@@ -1,37 +1,45 @@
-import C from './constants';
+import C from "./constants";
 // synchronous actions
 export const setPage = page => ({
-    type: C.SET_PAGE,
-    payload: page
-})
+  type: C.SET_PAGE,
+  payload: page
+});
 export const setName = name => ({
-    type: C.SET_NAME,
-    payload: name
-})
+  type: C.SET_NAME,
+  payload: name[0].toUpperCase() + name.slice(1)
+});
 
 //emit
-export const emitPostMessage = (message) => 
-   ({
-        type: "emit",
-        event: "postMessage",
-        message: message
-    })
+export const emitPostMessage = message => ({
+  type: "emit",
+  wsType: "emit",
+  event: "postMessage",
+  message: message
+});
 
-    
+export const emitOnline = name => ({
+  type: "emit",
+  wsType: "emit",
+  event: "online",
+  message: name
+});
 
-export const emitUpdateMessage = ( message) => ({
-    type: "emit",
-    event: "editMessage",
-    message: message
-})
+export const emitUpdateMessage = message => ({
+  type: "emit",
+  wsType: "emit",
+  event: "editMessage",
+  message: message
+});
 
-export const emitDeleteMessage = ( id) => ({
-    type: "emit",
-    event: "deleteMessage",
-    message: id
-})
+export const emitDeleteMessage = id => ({
+  type: "emit",
+  wsType: "emit",
+  event: "deleteMessage",
+  message: id
+});
 
 //helpers for on
+/*
 export const postMessage =  message => ({
     type: C.POST_MESSAGE,
     payload: message
@@ -51,34 +59,43 @@ export const onlineList = list => ({
     payload: list
 })
 
-
+*/
 //on
 
-export const onPostMessage = (message) => ({socket,dispatch}) => {
-    socket.on("serverSendPost", dispatch(postMessage(message)));
-}
-
-export const onDeleteMessage = (id) =>  ({socket,dispatch}) => {
-    socket.on("serverDeletePost", dispatch(deleteMessage(id)))
-}
-export const onEditMessage = (list) =>  ({socket,dispatch}) => {
-    socket.on("serverEditMessage", dispatch(updateMessage(list)))
-}
-
-export const onUpdateOnline = (list) =>  ({socket,dispatch}) => {
-    socket.on("updateOnline", dispatch(onlineList(list)))
-}
-
+export const onPostMessage = message => ({
+  wsType: "on",
+  type: C.POST_MESSAGE,
+  event: "serverSendPost",
+  payload: message
+});
+export const onDeleteMessage = id => ({
+  wsType: "on",
+  type: C.DELETE_MESSAGE,
+  event: "serverDeletePost",
+  payload: id
+});
+export const onEditMessage = message => ({
+  wsType: "on",
+  type: C.UPDATE_MESSAGE,
+  event: "serverEditMessage",
+  payload: message
+});
+export const onUpdateOnline = list => ({
+  wsType: "on",
+  type: C.UPDATE_ONLINE_LIST,
+  event: "updateOnline",
+  payload: list
+});
 
 //load innitial data
 export const fetchMessage = () => {
-    return dispatch => {
-        fetch("/getData")
-            .then(data => data.json())
-            .then(res => dispatch(loadMessage(res)))
-    }
-}
+  return dispatch => {
+    fetch("/getData")
+      .then(data => data.json())
+      .then(res => dispatch(loadMessage(res)));
+  };
+};
 export const loadMessage = messages => ({
-    type: C.GET_MESSAGE,
-    payload: messages
-})
+  type: C.GET_MESSAGE,
+  payload: messages
+});

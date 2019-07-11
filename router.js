085ce055c572a51ae30808932832cc2790f2b392
datngo1995 <datngotat1995@gmail.com>
2019-07-11 07:37:43 +0300

    Format code.

--- router.js
+++ router.js
@@ -1,18 +1,16 @@
 const express = require("express");
-const dbAction = require("./dbConnect").dbAction
+const dbAction = require("./dbConnect").dbAction;
 
+module.exports = router = async () => {
+  const mdb = await require("./dbConnect").connectDB();
+  const router = express.Router();
 
-
-module.exports = router = (async () => {
-  const mdb = await  require("./dbConnect").connectDB()
-const router = express.Router();
-
-router.get("/getData", async (req, res) => {
-    const content = await mdb 
-        .collection("chat-message")
-        .find()
-        .toArray()
-  res.send(content);
-});
+  router.get("/getData", async (req, res) => {
+    const content = await mdb
+      .collection("chat-message")
+      .find()
+      .toArray();
+    res.send(content);
+  });
   return router;
-});
+};
--- server.js
+++ server.js
@@ -1,5 +1,5 @@
 const express = require("express");
-const dbAction = require("./dbConnect").dbAction
+const dbAction = require("./dbConnect").dbAction;
 let mdb = null;
 const cors = require("cors");
 const bodyParser = require("body-parser");
@@ -26,64 +26,61 @@ if (process.env.NODE_ENV === "production") {
   });
 }
 
-
-
-
 io.on("connection", socket => {
   console.log("user connected");
   let name = null;
   socket.on("online", async newUserName => {
     name = newUserName;
-    const  userList = await mdb
+    const userList = await mdb
       .collection("users")
       .find()
       .toArray();
-      const  isNewUser = !userList.some(users => users.user === newUserName);
+    const isNewUser = !userList.some(users => users.user === newUserName);
     if (isNewUser)
       await mdb.collection("users").insertOne({
         user: name,
         status: "online"
-      }) 
+      });
     else
-      await  mdb.collection("users").updateOne(
+      await mdb.collection("users").updateOne(
         { user: newUserName },
         {
           $set: {
             status: "online"
           }
         }
-      )
-      const  onlineList =  await mdb
+      );
+    const onlineList = await mdb
       .collection("users")
       .find()
-      .toArray()
-    io.emit("updateOnline", onlineList );
+      .toArray();
+    io.emit("updateOnline", onlineList);
   });
   socket.on("disconnect", async () => {
     console.log("user disconnected");
-    await  mdb.collection("users").updateOne(
+    await mdb.collection("users").updateOne(
       { user: name },
       {
         $set: {
           status: "offline"
         }
       }
-    )
+    );
     let onlineList = await mdb
       .collection("users")
       .find()
-      .toArray()
+      .toArray();
     socket.emit("updateOnline", onlineList);
   });
   socket.on("postMessage", async message => {
-    console.log("postMessage", message)
-    const uuidv1 = require('uuid/v1')();
+    console.log("postMessage", message);
+    const uuidv1 = require("uuid/v1")();
 
-    await  mdb.collection("chat-message").insertOne({...message, id: uuidv1})
+    await mdb.collection("chat-message").insertOne({ ...message, id: uuidv1 });
     io.emit("serverSendPost", message);
   });
   socket.on("editMessage", async message => {
-    console.log("editMessage",message)
+    console.log("editMessage", message);
     await mdb.collection("chat-message").updateOne(
       { id: message.id },
       {
@@ -91,31 +88,20 @@ io.on("connection", socket => {
           content: message.content
         }
       }
-    )
-    io.emit("serverEditMessage", message)
+    );
+    io.emit("serverEditMessage", message);
   });
   socket.on("deleteMessage", async id => {
-    await mdb.collection("chat-message").deleteOne({ id })
+    await mdb.collection("chat-message").deleteOne({ id });
     io.emit("serverDeletePost", id);
   });
-  
 });
 
 (async () => {
-  
-  mdb = await require("./dbConnect").connectDB()
+  mdb = await require("./dbConnect").connectDB();
   const router = await require("./router")();
   server.use("/", router);
   ioServer.listen(port, () =>
-  console.log("Server connected with IO Socket available"))
-}
- 
-)();
-  
-
-
-
-
-
-
-  
+    console.log("Server connected with IO Socket available")
+  );
+})();

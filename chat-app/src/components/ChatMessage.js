917474df3746333391bf5d7ac94791a5a37721c6
datngo1995 <datngotat1995@gmail.com>
2019-07-18 04:59:45 +0300

    Fix margin for Chat Message.

--- chat-app/src/components/ChatMessage.js
+++ chat-app/src/components/ChatMessage.js
@@ -1,28 +1,36 @@
-import React from "react";
+import React, { useRef, useEffect } from "react";
 import { connect } from "react-redux";
-import UserMessage from './UserMessage'
-const ChatMessage = ({messages,name}) => {
-  
+import UserMessage from "./UserMessage";
+import { loadOldMessage,fetchMessage } from "../actions";
+const ChatMessage = ({ limit, messages, name,loadOldMessage,fetchMessage }) => {
+  const chatBoxHeight = useRef(null);
+  useEffect(() => {
+    scrollToBottom(chatBoxHeight.current);
+  }, []);
+  useEffect(( ) => {
+    console.log(limit,name)
+    fetchMessage(limit)
+  }, [limit])
+  const scrollToBottom = element => {
+    element.scrollIntoView({block: "end"});
+  };
   return (
-    <React.Fragment>
-      {messages.map((message, i) => (
-       
-        (message.name === name) ? 
-          <UserMessage id = {message.id}/>
-          : (
-          <div className="chat-message d-flex flex-column mb-3">
+    <div className="messages m-5 d-flex flex-column" ref={chatBoxHeight}>
+      <button onClick = { loadOldMessage}>Load older messages ...</button>
+      {messages.map((message, i) =>
+        message.name === name ? (
+          <UserMessage id={message.id} />
+        ) : (
+          <div className="chat-message d-flex flex-column m-3">
             <p>
               {message.name}: {message.content}
             </p>
             <p className="message-date"> {message.date} </p>
           </div>
         )
-      ))}
-     
-    </React.Fragment>
+      )}
+    </div>
   );
 };
 
-export default connect(
-  state => state
-)(ChatMessage);
+export default connect(state => state, {loadOldMessage, fetchMessage})(ChatMessage);

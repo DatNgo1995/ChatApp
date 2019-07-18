438d665d6d8063ba7345721e93f50cd6de094adb
datngo1995 <datngotat1995@gmail.com>
2019-07-18 07:31:38 +0300

    Distinguish loading old messages and posting new message.

--- chat-app/src/components/ChatMessage.js
+++ chat-app/src/components/ChatMessage.js
@@ -1,29 +1,46 @@
-import React, { useState,useRef, useEffect } from "react";
+import React, { useState, useRef, useEffect } from "react";
 import { connect } from "react-redux";
 import UserMessage from "./UserMessage";
-import { loadOldMessage,fetchMessage } from "../actions";
-const ChatMessage = ({ limit, messages, name,loadOldMessage,fetchMessage }) => {
-  const [messageState, setMessageState] = useState(messages)
+import { loadOldMessage, fetchMessage } from "../actions";
+const ChatMessage = ({
+  limit,
+  messages,
+  name,
+  loadOldMessage,
+  fetchMessage
+}) => {
+  const [messageState, setMessageState] = useState(messages);
+  const [loadDone, setLoadDone] = useState(false);
   const chatBoxHeight = useRef(null);
   useEffect(() => {
     scrollToBottom(chatBoxHeight.current);
   }, []);
   useEffect(() => {
-    console.log(messages.length - messageState.length)
     // not scroll to bottom when loading old messages
-    if (messages.length - messageState.length !== 10)
+    if (
+      messages.length - messageState.length === 1 &&
+      messages[0] === messageState[0]
+    )
       scrollToBottom(chatBoxHeight.current);
-    setMessageState(messages)
+    let loadDoneCondition =
+      (messages.length - messageState.length < 10 &&
+        messages.length - messageState.length > 1) ||
+      (messages.length - messageState.length === 1 &&
+        messages[0] !== messageState[0]);
+    if (loadDoneCondition) setLoadDone(true);
+    setMessageState(messages);
   }, [messages]);
-  useEffect(( ) => {
-    fetchMessage(limit)
-  }, [limit])
+  useEffect(() => {
+    fetchMessage(limit);
+  }, [limit]);
   const scrollToBottom = div => {
     div.scrollTop = div.scrollHeight - div.clientHeight;
   };
   return (
     <div className="messages m-5 d-flex flex-column" ref={chatBoxHeight}>
-      <button onClick = { loadOldMessage}>Load older messages ...</button>
+      {loadDone ? null : (
+        <button onClick={loadOldMessage}>Load older messages ...</button>
+      )}
       {messages.map((message, i) =>
         message.name === name ? (
           <UserMessage id={message.id} />
@@ -37,8 +54,10 @@ const ChatMessage = ({ limit, messages, name,loadOldMessage,fetchMessage }) => {
         )
       )}
     </div>
-    
   );
 };
 
-export default connect(state => state, {loadOldMessage, fetchMessage})(ChatMessage);
+export default connect(
+  state => state,
+  { loadOldMessage, fetchMessage }
+)(ChatMessage);
